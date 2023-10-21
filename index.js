fbd90316669cecad82a97358fc467dee06f8a60e
rnk-src <rounak.rai@gmail.com>
2023-10-21 09:46:04 -0500

    added gravity!

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ let windowWidth = window.innerWidth;
 canvas.width = windowWidth;
 canvas.height = windowHeight;
 
-canvas.style.background = "#ff8";
+canvas.style.background = "#000000";
 
 class Circle {
-    constructor (xPosition, yPosition, radius, color) {
+    constructor (xPosition, yPosition, radius, color, speed) {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.radius = radius;
         this.color = color;
+        this.speed = speed;
+
+        this.dx = 1 * speed;
+        this.dy = 1 * speed;
     }
 
     draw (context) {
@@ -27,14 +31,56 @@ class Circle {
         context.fill();
         context.closePath();
     }
+
+    update () {
+        context.clearRect(0, 0, windowWidth, windowHeight);
+
+        this.draw(context);
+
+        let sides = {leftMost: this.xPosition - this.radius, rightMost: this.xPosition + this.radius,
+            bottomMost: this.yPosition + this.radius, topMost: this.yPosition - this.radius};
+
+        if (sides.rightMost >= windowWidth || sides.leftMost < 0) {
+            this.dx = -this.dx * 0.9;
+            if (sides.rightMost >= windowWidth) {
+                this.xPosition = windowWidth - this.radius;
+            } else {
+                this.xPosition = this.radius;
+            }
+        }
+        if (sides.bottomMost >= windowHeight || sides.topMost < 0) {
+            this.dy = -this.dy * 0.9;
+            if (sides.bottomMost >= windowHeight) {
+                this.yPosition = windowHeight - this.radius;
+            } else {
+                this.yPosition = this.radius;
+            }
+        }
+
+        this.dy += 0.2
+
+        this.xPosition += this.dx;
+        this.yPosition += this.dy;
+    }
 }
 
-let allCircles = [];
+// let allCircles = [];
+//
+// for (let numbers = 0; numbers < 10; numbers++) {
+//     let randomX = Math.random() * windowWidth;
+//     let randomY = Math.random() * windowHeight;
+//
+//     allCircles.push(new Circle(randomX, randomY, 50, "red"));
+//     allCircles[numbers].draw(context);
+// }
 
-for (let numbers = 0; numbers < 10; numbers++) {
-    let randomX = Math.random() * windowWidth;
-    let randomY = Math.random() * windowHeight;
+let ball = new Circle(100, 100, 50, "red", 30);
 
-    allCircles.push(new Circle(randomX, randomY, 50, "red"));
-    allCircles[numbers].draw(context);
+ball.draw(context);
+
+let updateCircle = function () {
+    requestAnimationFrame(updateCircle);
+    ball.update();
 }
+
+updateCircle();
