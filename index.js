222e0849be9e6fb34aac6a694a9935a1cf697d1f
rnk-src <rounak.rai@gmail.com>
2023-10-20 22:57:03 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("canvas");
+
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+canvas.style.background = "#ff8";
